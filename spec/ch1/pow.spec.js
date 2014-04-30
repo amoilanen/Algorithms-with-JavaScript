7ddbafe104b16318c855122f710d8502b358df7a
anton <anton.al.ivanov@gmail.com>
2014-05-01 00:16:58 +0200

    Implemented both slow and fast power algorithms

--- spec/ch1/pow.spec.js
+++ spec/ch1/pow.spec.js
@@ -1,18 +1,21 @@
 describe("pow", function() {
 
-  it("should keep 1 unchanged", function() {
-    expect(ch1.pow(1, 4)).toBe(1);
-  });
+  ["powSlow", "pow"].forEach(function(funcName) {
 
-  it("should be 1 when the power is 0", function() {
-    expect(ch1.pow(3, 0)).toBe(1);
-  });
+    it(funcName + " should keep 1 unchanged", function() {
+      expect(ch1[funcName](1, 4)).toBe(1);
+    });
 
-  it("should compute the odd power", function() {
-    expect(ch1.pow(5, 3)).toBe(125);
-  });
+    it(funcName + " should be 1 when the power is 0", function() {
+      expect(ch1[funcName](3, 0)).toBe(1);
+    });
 
-  it("should compute the even power", function() {
-    expect(ch1.pow(2, 10)).toBe(1024);
+    it(funcName + " should compute the odd power", function() {
+      expect(ch1[funcName](5, 3)).toBe(125);
+    });
+
+    it(funcName + " should compute the even power", function() {
+      expect(ch1[funcName](2, 10)).toBe(1024);
+    });
   });
 });
