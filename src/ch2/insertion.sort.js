e6efacb799e42e984f99320fc0cff4301af3cf91
anton <anton.al.ivanov@gmail.com>
2014-06-08 21:34:15 +0300

    Implemented insertion sort

---
+++ src/ch2/insertion.sort.js
@@ -0,0 +1,23 @@
+(function(host) {
+
+  function insertionSort(elements) {
+    var copy = elements.slice(0);
+    var toInsert = null;
+    var insertIndex = -1;
+
+    for (var i = 1; i < copy.length; i++) {
+      toInsert = copy[i];
+      insertIndex = i - 1;
+      while ((insertIndex >= 0) && (toInsert < copy[insertIndex])) {
+        copy[insertIndex + 1] = copy[insertIndex];
+        insertIndex--;
+      }
+      insertIndex++;
+      copy[insertIndex] = toInsert;
+    }
+    return copy;
+  }
+
+  host.ch2 = host.ch2 || {};
+  host.ch2.insertionSort = insertionSort;
+})(this);
