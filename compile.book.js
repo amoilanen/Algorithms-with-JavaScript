7138a9057eb76bfe1303bc3dfd0bf1e57e3be408
Anton Ivanov <anton.al.ivanov@gmail.com>
2016-11-10 23:51:07 +0200

    Build support for automatically compiling the book from the markdown sources with pandoc.

---
+++ compile.book.js
@@ -0,0 +1,27 @@
+const chokidar = require('chokidar');
+const path = require('path');
+require('shelljs/global');
+
+function prepareOutputDir() {
+  rm('-rf', 'book/html');
+  mkdir('-p', 'book/html');
+}
+
+function compile(file) {
+  const fileBasename = path.basename(file, '.md');
+  const outputFile = 'book/html/' + fileBasename + '.html';
+  const timestamp = new Date().toISOString();
+
+  console.log('[' + timestamp + ']' + file + ' -> ' + outputFile);
+  console.log('pandoc ' + file + ' -c pandoc.css -o ' + outputFile);
+  exec('pandoc ' + file + ' -c pandoc.css -o ' + outputFile);
+}
+
+console.log('Setting up output directory...');
+prepareOutputDir();
+console.log('Compiling files...');
+ls('book/*.md')
+  .forEach(file => compile(file));
+console.log('Watching .md files for changes...');
+chokidar.watch('book/*.md')
+  .on('change', file => compile(file));
